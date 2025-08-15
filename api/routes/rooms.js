d10ac7f7e66314e11d769e5cd75d4dbcb98eed27
ErtaKryeziu <ek67377@ubt-uni.net>
2025-08-16 01:19:29 +0200

    Added Room model, controller, and routes for CRUD operations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import { createRoom, updateRoom, deleteRoom, getRoom, getAllRooms } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+// CREATE new room
+router.post("/:hotelid", verifyAdmin, createRoom); 
 
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getAllRooms);
 
 export default router;
