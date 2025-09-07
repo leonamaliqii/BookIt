4e2339e8b97117e77111bcc262475fa61b1f3593
Erta Kryeziu <ek67377@ubt-uni.net>
2025-09-07 20:07:06 +0200

    Updated Navbar component with icons and design

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar/>
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+      <Navbar />
+      home container
+
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
