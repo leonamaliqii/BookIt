1eee04408f1357832be59377e993726041ccfc54
ErtaKryeziu <ek67377@ubt-uni.net>
2025-09-28 03:43:00 +0200

    Added logout button to client Navbar and styled it

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,31 +1,45 @@
 import "./navbar.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
-const Navbar = () => {
-   const { user } = useContext(AuthContext);
 
-   
+const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">BookIt</span>
         </Link>
-      { user ? user.username : (
-        <div className="navItems">
-          <Link to="/register" style={{color:"inherit", textDecoration:"none"}}>
-            <button className="navButton">Register</button>
-          </Link>
-          <Link to="/login" style={{color:"inherit", textDecoration:"none"}}>
-            <button className="navButton">Login</button>
-          </Link>
-        </div>)}
+
+        {user ? (
+          <div className="navItems">
+            <span className="username">{user.username}</span>
+            <button className="headerBtn" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <Link to="/register" style={{ color: "inherit", textDecoration: "none" }}>
+              <button className="headerBtn">Register</button>
+            </Link>
+            <Link to="/login" style={{ color: "inherit", textDecoration: "none" }}>
+              <button className="headerBtn">Login</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Navbar;
-
-
